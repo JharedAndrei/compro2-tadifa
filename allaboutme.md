167d9e53c1656b707c430d767a7c7797a397a469
JharedAndrei <jharedandrei.tadifa@lorma.edu>
2026-01-13 11:50:09 +0800

    array mo pakak

--- allaboutme.md
+++
@@ -1,5 +0,0 @@
-*I am Jhared Andrei*
-
-*pinaka pogi sa carlatan*
-
-*yon laang lab yo.*
---
+++ arraymopakak.java
@@ -0,0 +1,15 @@
+public class ArrayMopakak {
+    public static void main(String[] args) {
+        //1. Create an array of 10 numbers with any values
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       
+        //2. Display the array elements using a for loop
+        System.out.println(" Displaying Array elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
+
+   
